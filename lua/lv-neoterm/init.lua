46b9b9008d379d972d347af273a5f9984e019175
mattdube <xero.luck@gmail.com>
2021-06-15 06:47:03 -0500

    setup python lsp, installed and configured neoterm for interactive python terminal

---
+++ lua/lv-neoterm/init.lua
@@ -0,0 +1,5 @@
+vim.g.neoterm_default_mod = 'belowright'
+vim.g.neoterm_size = 20
+vim.g.neoterm_autoscroll = 1
+vim.api.nvim_set_keymap( 'n',  '<S-l>', ':TREPLSendLine<CR><CR>', {noremap = true})
+vim.api.nvim_set_keymap( 'v',  '<S-l>', ':TREPLSendSelection<CR><CR>', {noremap = true})
